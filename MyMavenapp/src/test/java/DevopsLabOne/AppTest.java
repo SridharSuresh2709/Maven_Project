ee2dff5b7d332df88628bc39cc3c8ad75cf69fc7
SridharSuresh2709 <155534169+SridharSuresh2709@users.noreply.github.com>
2025-06-06 17:15:49 +0530

    AppTest.java

--- MyMavenapp/src/test/java/DevopsLabOne/AppTest.java
+++ MyMavenapp/src/test/java/DevopsLabOne/AppTest.java
@@ -1,9 +1,22 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+package DevopsLabOne;
 
-public class SampleFailingTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
     @Test
-    public void testShouldFail() {
-        assertEquals(1, 2); // this will fail
+    public void shouldAnswerWithTrue() {
+        App app = new App();
+        String message = app.getmessage();
+        assertEquals("Hello Sridhar!", message);
+        System.out.println("Test passed: " +message);
     }
 }
